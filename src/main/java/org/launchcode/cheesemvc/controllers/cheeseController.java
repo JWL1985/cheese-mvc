99c80db3f1b9dcbd7dfb48bf890a8d2bf341ad4d
John Lohmann <johnlohmann@gmail.com>
2019-06-10 08:38:14 -0500

    studio 3.4 complete - remove cheeses

--- src/main/java/org/launchcode/cheesemvc/controllers/cheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/cheeseController.java
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
-@RequestMapping(value= "cheese")
+@RequestMapping(value= "/cheese")
 public class cheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String, String> cheeses = new HashMap<>();
 
     @RequestMapping(value= "")
     public String index(Model model) {
@@ -21,18 +22,34 @@ public class cheeseController {
 
         model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
-        return "cheese/index";
+        return "/cheese/index";
     }
 
     @RequestMapping(value="add", method = RequestMethod.GET)
     public String displayAddCheeseForm(Model model) {
         model.addAttribute("title", "Add Cheese");
-        return "cheese/add";
+        return "/cheese/add";
 
     }
     @RequestMapping(value="add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName) {
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
+        cheeses.put(cheeseName, cheeseDescription);
         return "redirect:";
     }
+
+    @RequestMapping(value="remove", method = RequestMethod.GET)
+    public String removeCheese(Model model) {
+        model.addAttribute("cheeses", cheeses);
+        return "/cheese/remove";
+    }
+
+    @RequestMapping(value="remove", method = RequestMethod.POST)
+    public String processRemoveCheese(@RequestParam ArrayList<String> cheese) {
+        for(int i = 0; i < cheese.size(); i++) {
+            cheeses.remove(cheese.get(i));
+        }
+        return "/cheese/remove";
+    }
+
+
 }
